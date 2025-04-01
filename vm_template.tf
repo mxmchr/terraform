e52797f5ce6687f322731d502bbeef2a46979094
root <root@cli-maxime.esgi4.lan>
2025-04-01 16:43:45 +0200

    Fix: ajout de Node.js pour Terraform

---
+++ vm_template.tf
@@ -0,0 +1,46 @@
+resource "proxmox_vm_qemu" "vm" {
+  name        = "ubuntu-vm"
+  target_node = "pev1"  # Remplacez par le nom de votre nœud Proxmox si différent
+
+  # Utilisation du template ID 9000
+  clone = "ubuntu-24-cloudinit"  # Clonage depuis le template existant
+
+  # Configuration matérielle
+  cores   = 2
+  sockets = 1
+  memory  = 2048
+
+  # Configuration du contrôleur SCSI en virtio-scsi-single
+  scsihw = "virtio-scsi-single"
+
+  # Configuration disque
+  disk {
+    size    = "20G"
+    type    = "scsi"
+    storage = "local"  # Utilisation du stockage local-lvm
+  }
+
+  # Configuration réseau
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  # Activation de Cloud-Init
+  ipconfig0 = "dhcp"
+
+  # Configuration d'amorçage
+  boot = "order=scsi0;ide2"
+
+  # Paramètres additionnels
+  agent  = 1
+  onboot = true
+}
+
+output "vm_id" {
+  value = proxmox_vm_qemu.vm.id
+}
+
+output "vm_name" {
+  value = proxmox_vm_qemu.vm.name
+}
